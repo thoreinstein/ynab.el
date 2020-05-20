a12f89d32ffdeb2dd7d56878fb908a8159cf14b7
janders223 <jimanders223@gmail.com>
2020-05-20 09:06:36 -0400

    Initial commit

---
+++ ynab.el
@@ -0,0 +1,38 @@
+;;; ynab.el --- YNAB... in Emacs! -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2020 Jim Anders
+;;
+;; Author: Jim Anders <http://github.com/janders223>
+;; Maintainer: Jim Anders <jimanders223@gmail.com>
+;; Created: May 20, 2020
+;; Modified: May 20, 2020
+;; Version: 0.0.1
+;; Keywords: YNAB
+;; Homepage: https://github.com/janders223/ynab.el
+;; Package-Requires: ((emacs 26.3) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; Major mode for working with your YNAB data
+;;
+;;; Code:
+
+
+
+(provide 'ynab)
+;;; ynab.el ends here
